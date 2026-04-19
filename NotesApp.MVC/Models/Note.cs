1062d2aae79d81fbcba5cc185f7fcba4b49dcde9
Ruslan Savchyn <Ruslannsvv>
2026-04-19 19:10:03 +0300

    Add Note model

---
+++ NotesApp.MVC/Models/Note.cs
@@ -0,0 +1,14 @@
+﻿
+namespace NotesApp.MVC.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+        public List<string> Tags { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt  { get; set; }
+    }
+}
