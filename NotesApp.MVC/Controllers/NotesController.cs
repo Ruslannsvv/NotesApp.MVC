3be4227a1671681e87534f57a9da6f420525e457
Ruslan Savchyn <Ruslannsvv>
2026-04-20 13:48:37 +0300

    Fix: add missing Create POST logic

--- NotesApp.MVC/Controllers/NotesController.cs
+++ NotesApp.MVC/Controllers/NotesController.cs
@@ -14,6 +14,24 @@ namespace NotesApp.MVC.Controllers
             return View(notes);
 
         }
+
+
+        [HttpGet]
+        public IActionResult Create() { 
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string title, string content, string tags)
+        {
+            var tagList = tags != null && tags != ""
+                ? tags.Split(',').Select(t => t.Trim()).ToList()
+                : new List<string>();
+
+            noteService.AddNote(title, content, tagList);
+
+            return RedirectToAction("Index");
+        }
     }
     
 
