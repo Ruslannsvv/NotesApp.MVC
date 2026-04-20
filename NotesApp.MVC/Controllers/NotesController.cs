dd30b08975eb7490939ccdb0fcdc60731dc273ec
Ruslan Savchyn <Ruslannsvv>
2026-04-20 13:45:12 +0300

    Implement Create action(POST) with tag parsing

---
+++ NotesApp.MVC/Controllers/NotesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotesApp.MVC.Models;
+using NotesApp.MVC.Services;
+
+namespace NotesApp.MVC.Controllers
+{
+    public class NotesController : Controller
+    {
+        NoteService noteService = new NoteService();
+
+        public IActionResult Index()
+        {
+            var notes = noteService.GetAllNotes();
+            return View(notes);
+
+        }
+    }
+    
+
+}
